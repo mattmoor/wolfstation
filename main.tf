9087121370ab83c84358f0fa95d6b7d0d9e31789
Matt Moore <mattmoor@chainguard.dev>
2023-07-20 09:14:09 -0700

    Shift away from the apko.yaml so we can leverage HCL to build up cmd with comments
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>

--- main.tf
+++ main.tf
@@ -5,15 +5,9 @@ SPDX-License-Identifier: Apache-2.0
 
 terraform {
   required_providers {
-    apko = {
-      source = "chainguard-dev/apko"
-    }
-    cosign = {
-      source = "chainguard-dev/cosign"
-    }
-    oci = {
-      source = "chainguard-dev/oci"
-    }
+    apko   = { source = "chainguard-dev/apko" }
+    cosign = { source = "chainguard-dev/cosign" }
+    oci    = { source = "chainguard-dev/oci" }
   }
 }
 
@@ -28,11 +22,50 @@ provider "apko" {
   extra_packages     = ["wolfi-baselayout"]
 }
 
+locals {
+  startup_commands = [
+    # Create a user named "user" with no password and passwordless sudo access.
+    "(adduser -D user -s /bin/bash)",
+    "passwd -d user",
+    # TODO(mattmoor): This doesn't quite work yet, JO thinks it has to do with a PAM issue.
+    "(echo 'user            ALL = (ALL) NOPASSWD: ALL' >> /etc/sudoers)",
+
+    # Set up and launch sshd, by setting up the host keys and config that allow for empty passwords.
+    "(yes | ssh-keygen -q -f /etc/ssh/ssh_host_rsa_key -t rsa -C 'host' -N '' )",
+    "mkdir /var/empty",
+    "(echo 'PermitEmptyPasswords yes' > /etc/ssh/sshd_config)",
+    "/usr/sbin/sshd",
+
+    # Cloud workstations require the container entrypoint to block forever.
+    "sleep infinity",
+  ]
+}
+
 module "image" {
   source  = "chainguard-dev/apko/publisher"
 
   target_repository = var.target_repository
-  config = file("${path.module}/apko.yaml")
+  config = jsonencode({
+    contents = {
+      packages = [
+        # This is so we have a shell and posix tools
+        "busybox",
+
+        # This is how Cloud Workstations connects to the container.
+        "openssh-server",
+
+        # To do stuff with GCP (this is super fat)
+        "google-cloud-sdk",
+
+        # TODO(mattmoor): Get this working, but sudo-rs and PAM don't get along.
+        "sudo-rs",
+      ]
+    }
+    entrypoint = {
+      command = "/bin/sh"
+    }
+    cmd = "-c '${join("&&", local.startup_commands)}'"
+  })
 }
 
 resource "oci_tag" "latest" {
